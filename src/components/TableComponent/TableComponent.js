878482eadd013bda76422537e19aef6b5b438e30
mahn-dev <mahndev02@gmail.com>
2023-08-03 08:21:41 +0700

    drawer, input, modal, table component

---
+++ src/components/TableComponent/TableComponent.js
@@ -0,0 +1,31 @@
+import { Table } from 'antd';
+
+function TableComponent(props) {
+    const { selectionType = 'checkbox', data = [], columns = [] } = props;
+
+    const rowSelection = {
+        onChange: (selectedRowKeys, selectedRows) => {
+            console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+        },
+        getCheckboxProps: (record) => ({
+            disabled: record.name === 'Disabled User',
+            // Column configuration not to be checked
+            name: record.name,
+        }),
+    };
+    return (
+        <div>
+            <Table
+                rowSelection={{
+                    type: selectionType,
+                    ...rowSelection,
+                }}
+                columns={columns}
+                dataSource={data}
+                {...props}
+            />
+        </div>
+    );
+}
+
+export default TableComponent;
