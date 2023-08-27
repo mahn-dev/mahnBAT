5142d9e42539df9d38e4bbcde86d22fa18be5274
mahn-dev <mahndev02@gmail.com>
2023-08-27 21:28:43 +0700

    install recharts, redux-persist

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import App from '~/App';
 import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import reportWebVitals from './reportWebVitals';
 
 import GlobalStyles from '~/components/GlobalStyles';
-import { store } from '~/redux/store';
+import { store, persistor } from '~/redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -17,9 +18,11 @@ root.render(
     // <React.StrictMode>
     <QueryClientProvider client={queryClient}>
         <Provider store={store}>
-            <GlobalStyles>
-                <App />
-            </GlobalStyles>
+            <PersistGate loading={null} persistor={persistor}>
+                <GlobalStyles>
+                    <App />
+                </GlobalStyles>
+            </PersistGate>
         </Provider>
         <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>,
